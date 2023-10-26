5b3c23500c11ac34b19b47ce67181f3d63920404
Tyler Johnston <johnstontyler@protonmail.com>
2023-10-25 22:13:30 -0600

    finished problem 1 part 2

--- assn5.py
+++ assn5.py
@@ -2,18 +2,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 import cv2
 
-# PROBLEM 1 PART "A"
 wirebondIm = cv2.imread('Wirebond.tif', cv2.IMREAD_GRAYSCALE)
+shapesIm = cv2.imread('Shapes.tif', cv2.IMREAD_GRAYSCALE)
 
-# PROBLEM 1 PART "B"
-rectangleErode_SE_B = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (20,20))
-erodedRectagleIm_B = cv2.morphologyEx(wirebondIm, cv2.MORPH_ERODE, rectangleErode_SE_B)
+# PROBLEM 1 QUESTION 1B
+ellipseErode_SE_B = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (20,20))
+erodedEllipseIm_B = cv2.morphologyEx(wirebondIm, cv2.MORPH_ERODE, ellipseErode_SE_B)
 
-# PROBLEM 1 PART "C"
+# PROBLEM 1 QUESTION 1C
 rectangleErode_SE_C = cv2.getStructuringElement(cv2.MORPH_RECT, (8,8))
 erodedRectagleIm_C = cv2.morphologyEx(wirebondIm, cv2.MORPH_ERODE, rectangleErode_SE_C)
 
-# PROBLEM 1 PART "D"
+# PROBLEM 1 QUESTION 1D
 rectangleErode_SE_D = cv2.getStructuringElement(cv2.MORPH_RECT, (35,35))
 erodedRectagleIm_D = cv2.morphologyEx(wirebondIm, cv2.MORPH_ERODE, rectangleErode_SE_D)
 
@@ -26,7 +26,7 @@ plt.axis("off")
 plt.title("A")
 
 plt.subplot(1, 4, 2)
-plt.imshow(erodedRectagleIm_B, cmap='gray')
+plt.imshow(erodedEllipseIm_B, cmap='gray')
 plt.axis("off")
 plt.title("B")
 
@@ -39,6 +39,45 @@ plt.subplot(1, 4, 4)
 plt.imshow(erodedRectagleIm_D, cmap='gray')
 plt.axis("off")
 plt.title("D")
+plt.tight_layout()
+
+# PROBLEM 1 QUESTION 1F
+rectanlgeOpenSE_F = cv2.getStructuringElement(cv2.MORPH_RECT, (20,20))
+rectanlgeOpenIm_F = cv2.morphologyEx(shapesIm, cv2.MORPH_OPEN, rectanlgeOpenSE_F)
+
+# PROBLEM 1 QUESTION 1G
+rectanlgeCloseSE_G = cv2.getStructuringElement(cv2.MORPH_RECT, (20,20))
+rectanlgeCloseIm_G = cv2.morphologyEx(shapesIm, cv2.MORPH_CLOSE, rectanlgeCloseSE_G)
+
+# PROBLEM 1 QUESTION 1H
+rectangleDilate_SE_H = cv2.getStructuringElement(cv2.MORPH_RECT, (18,18))
+dilatedRectagleIm_H = cv2.morphologyEx(rectanlgeOpenIm_F, cv2.MORPH_DILATE, rectangleDilate_SE_H)
+
+
+
+
+# plotting
+plt.figure(figsize=(10, 5)) # Figure 1
+plt.suptitle("Problem 1, Part 2: Wirebond.tif morphological operations")
+plt.subplot(1, 4, 1)
+plt.imshow(shapesIm, cmap='gray')
+plt.axis("off")
+plt.title("E")
+
+plt.subplot(1, 4, 2)
+plt.imshow(rectanlgeOpenIm_F, cmap='gray')
+plt.axis("off")
+plt.title("F")
+
+plt.subplot(1, 4, 3)
+plt.imshow(rectanlgeCloseIm_G, cmap='gray')
+plt.axis("off")
+plt.title("G")
+
+plt.subplot(1, 4, 4)
+plt.imshow(dilatedRectagleIm_H, cmap='gray')
+plt.axis("off")
+plt.title("H")
 plt.tight_layout()
 
 plt.show()
